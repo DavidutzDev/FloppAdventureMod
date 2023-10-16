3249d5b35884e13e27ec8f6ae5d7b96263a5aa6e
DavidutzDev <david.gheghea@gmail.com>
2023-10-16 21:47:18 +0200

    :pencil:(RPC) Implemented discord rpc integration

--- src/main/java/fr/davidutz/floppadventuremod/client/ClientManager.java
+++ src/main/java/fr/davidutz/floppadventuremod/client/ClientManager.java
@@ -1,5 +1,7 @@
 package fr.davidutz.floppadventuremod.client;
 
+import fr.davidutz.floppadventuremod.FloppAdventureMod;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -8,11 +10,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @OnlyIn(Dist.CLIENT)
 public class ClientManager {
 
+    private final RPCManager rpcManager = new RPCManager();
+
     public void init() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     }
 
     private void clientSetup(final FMLClientSetupEvent e) {
-
+        this.rpcManager.startRPC(Minecraft.getInstance());
     }
 }
